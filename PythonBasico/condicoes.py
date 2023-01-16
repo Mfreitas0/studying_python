650367b2347b92079c44ad8a57c5354060024fb3
Mfreitas0 <mfreitasc0@gmail.com>
2023-01-16 20:37:25 -0300

    Condicoes if, elif e else

---
+++ PythonBasico/condicoes.py
@@ -0,0 +1,14 @@
+"""
+CONDIÇÕES IF, ELIF E ELSE
+"""
+if False:
+    print("Se, Verdadeiro")
+
+    num_1 = 1
+elif True:
+    print("Se não, é verdadeiro")
+elif True:
+    print("Mais uma verdadeira")
+else:
+    print("outro Não é verdadeiro")
+print("Não faz parte do bloco")
