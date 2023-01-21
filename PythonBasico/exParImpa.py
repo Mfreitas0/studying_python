2f6aed7c2f33c842b7ea50e168235cf77aea9180
Mfreitas0 <mfreitasc0@gmail.com>
2023-01-20 21:35:06 -0300

    passholder & Exercico

---
+++ PythonBasico/exParImpa.py
@@ -0,0 +1,11 @@
+num = input("Digite um numero inteiro: ")
+
+restoDiv = int(num) % 2
+
+# print(restoDiv)
+
+if restoDiv == 0:
+    print(f'O numero {num} que Você digitou é PAR!')
+else:
+    print(f'O numero {num} que Você digitou é IMPAR!')
+    
---
+++ PythonBasico/placeholder.py
@@ -0,0 +1,9 @@
+#placeholder -- pass= para fazer o condigo processeguir em um determinado trecho que deveria haver codigo, porém, no momento ñ foi colocado.(as vezes para colocar depois)
+
+valor = False
+
+if valor:
+    pass #IRIA ESCREVER ALGO, MAS NESSE MOMENTO N FEZ.
+    ... #FAZ A MESMA COISA DO PASS, SEGURA O LUGAR PARA DEPOIS ESCREVER ALGO AI
+else:
+    print("tchau!")
